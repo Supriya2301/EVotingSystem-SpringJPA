cd06baaefc1a15ac14d6853b97cf00d540bbb783
Supriya <63453592+Supriya2301@users.noreply.github.com>
2023-12-29 22:56:15 -0800

    Add files via upload

---
+++ EVotingSystem/src/main/java/com/codingninjas/EVotingSystem/entities/Election.java
@@ -0,0 +1,37 @@
+package com.codingninjas.EVotingSystem.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Election {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long id;
+	
+	@Column(unique=true)
+	private String name;
+
+	public Election() {
+	}
+
+	public Election(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
